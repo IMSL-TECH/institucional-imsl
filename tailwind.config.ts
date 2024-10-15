9d41b92e41c50558f69bb4ba7c125b19d1fb7a5f
Marcos Ramos <marcosperes.contato@gmail.com>
2024-10-15 09:27:19 -0300

    Tailwind theme config hass been cleened

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,30 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors: {
-        white: "#ffff",
-        primary: "#247377",
-        green: "#1f6266",
-        "middle-green": "#21696d",
-        "dark-green": "#184a4d",
-        "dark-blue": "#162A65",
-        areaWhite: "#285572",
-        "light-green": "#037c82",
-      },
-      backgroundColor: {
-        white: "#f3f3f3",
-        primary: "#247377",
-        green: "#1f6266",
-        "middle-green": "#21696d",
-        "dark-green": "#184a4d",
-        "dark-blue": "#162A65",
-        areaWhite: "#285572",
-      },
+      backgroundImage: { },
+      colors: { },
+      backgroundColor: {},
     },
   },
   plugins: [],
